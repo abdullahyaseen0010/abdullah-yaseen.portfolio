3f0ed114a6e56bb082213e7ed33da544b2c3a326
Abdullah Yaseen <abdullah0010yaseen@gmail.com>
2026-09-21 09:37:37 +0500

    Update ProjectCard.tsx

--- src/components/Project/ProjectCard.tsx
+++ src/components/Project/ProjectCard.tsx
@@ -112,9 +112,6 @@ const ProjectCard = ({ project, isHovered, onHoverStart, onHoverEnd }: ProjectCa
           <ProjectLinks github={project.github} demo={project.demo} />
         </div>
 
-         <div className="relative z-10">
-          <ProjectLinks github={project.github} demo={project.demo} />
-        </div>
 
         {/* Animated border glow on hover */}
         <motion.div
